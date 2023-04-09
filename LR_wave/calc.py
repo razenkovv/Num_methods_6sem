1112bfde830ed8a99612bfbddbb540fb27abf58d
razenkovv <razenkovv>
2023-04-09 20:52:36 +0300

    correct

--- LR_wave/calc.py
+++ LR_wave/calc.py
@@ -74,8 +74,6 @@ def calc_without_writing(mesh, timesteps, pattern_type, startmark=0, u_0=np.nan,
         :return: значения в момент времени timesteps[-2] и timesteps[-1]
         """
 
-    errors_ = np.zeros(len(timesteps - 1))
-
     tau = timesteps[1] - timesteps[0]
     h = mesh[1] - mesh[0]
     if startmark == 0:
@@ -85,26 +83,22 @@ def calc_without_writing(mesh, timesteps, pattern_type, startmark=0, u_0=np.nan,
     if pattern_type == 'CROSS_1':
         if startmark == 0:
             u_1 = u_0 + header.f_0t(mesh) * tau
-        errors_[0] = header.error_estimation(u_1 - header.exact(mesh, timesteps[1]))
         for t in timesteps[1:-1]:
             u_2[1:-1] = (u_1[2:] - 2 * u_1[1:-1] + u_1[:-2]) * header.c * tau ** 2 / h ** 2 + header.f(mesh[1:-1], t) * tau ** 2 + 2 * u_1[1:-1] - u_0[1:-1]
             u_2[0] = (header.f_lft(t + tau) * h - header.a2 * u_2[1]) / (header.a1 * h - header.a2)
             u_2[-1] = (header.f_rgt(t + tau) * h + header.b2 * u_2[-2]) / (header.b1 * h + header.b2)
-            errors_[int(t/tau)] = header.error_estimation(u_2 - header.exact(mesh, t))
             u_0, u_1, u_2 = u_1, u_2, u_0
 
     elif pattern_type == 'CROSS_2':
         if startmark == 0:
             u_1 = u_0 + header.f_0t(mesh) * tau + (header.c * header.f_0xx(mesh) + header.f(mesh, 0.0)) * 0.5 * tau ** 2
-        errors_[0] = header.error_estimation(u_1 - header.exact(mesh, timesteps[1]))
         for t in timesteps[1:-1]:
             u_2[1:-1] = (u_1[2:] - 2 * u_1[1:-1] + u_1[:-2]) * header.c * tau ** 2 / h ** 2 + header.f(mesh[1:-1], t) * tau ** 2 + 2 * u_1[1:-1] - u_0[1:-1]
             u_2[0] = (header.f_lft(t + tau) * h - 2 * header.a2 * u_2[1] + 0.5 * header.a2 * u_2[2]) / (header.a1 * h - 1.5 * header.a2)
             u_2[-1] = (header.f_rgt(t + tau) * h + 2 * header.b2 * u_2[-2] - 0.5 * header.b2 * u_2[-3]) / (header.b1 * h + 1.5 * header.b2)
-            errors_[int(t / tau)] = header.error_estimation(u_2 - header.exact(mesh, t))
             u_0, u_1, u_2 = u_1, u_2, u_0
 
     else:
         raise (Exception('No such method'))
 
-    return u_0, u_1, header.error_estimation(errors_)
+    return u_0, u_1
--- LR_wave/error.py
+++ LR_wave/error.py
@@ -8,24 +8,27 @@ import header
 import calc
 
 hsteps = [0.05 * 0.5 ** k for k in range(8)]
-#hsteps = [0.01, 0.001]
 df = pd.DataFrame(index=range(len(hsteps)), columns=['h', 'error'])
 
 for j, h in enumerate(hsteps):
-    tau = header.courant * h / np.sqrt(header.c)
-    mesh = np.arange(header.lft, header.rgt, h)
-    timesteps = np.arange(header.t_start, header.t_end, tau)
-    calc.calc_without_writing()
+    mesh = np.linspace(header.lft, header.rgt, int((header.rgt - header.lft) / h) + 1)
+    timesteps = np.arange(header.t_start, header.t_end, header.courant * h / np.sqrt(header.c))
+    errors = np.zeros_like(timesteps)
+    u_0, u_1 = calc.calc_without_writing(mesh, timesteps[0:2], pattern_type=header.method)
+    errors[1] = header.error_estimation(u_1 - header.exact(mesh, timesteps[1]))
 
+    for i, t in enumerate(timesteps[1:-1]):
+        u_0, u_1 = calc.calc_without_writing(mesh, timesteps[i:i+3], pattern_type=header.method, startmark=1, u_0=u_0, u_1=u_1)
+        errors[i+2] = header.error_estimation(u_1 - header.exact(mesh, timesteps[i+2]))
+    error = header.error_estimation(errors)
+    df.iloc[j] = h, error
 
-# df.iloc[j] = h, error
-# df = pd.read_csv(f'graphs/errors_{header.method}_{header.func}_c={header.courant}.csv', index_col=0)
 
 model = LinearRegression().fit(np.log(df['h'].to_numpy(dtype=float)).reshape(-1, 1), np.log(df['error'].to_numpy(dtype=float)))
 df.plot(figsize=(8, 8), x='h', y='error', loglog=True, marker='P', title=f'Errors_{header.method}_c={header.courant}')
 plt.text(0.7, 0.1, f'mean_tan={model.coef_[0]:.3f}', transform=plt.gca().transAxes)
 
-k = 1
+k = 0
 if k:
     print(f'{header.method}')
     print(f'mean_tan = {model.coef_[0]:.3f}')
